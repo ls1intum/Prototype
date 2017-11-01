0b99871308442c004e93f9e3ef0ffd125d3e0c3e
Paul Schmiedmayer <paul.schmiedmayer@tum.de>
2017-11-01 12:29:54 +0100

    Add Example Project

---
+++ Example/Prototype Example macOS/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Prototype Example macOS
+//
+//  Created by Paul Schmiedmayer on 7/15/17.
+//  Copyright © 2017 Paul Schmiedmayer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+}
+
